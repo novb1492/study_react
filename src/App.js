9b37f4a61afa135c3c6da11df2b488322e85fe0b
kimjunyoung <kimjy80@sesisoft.com>
2022-01-11 14:08:30 +0900

    props이용해 동적으로 할당

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class Subject extends Component{
   render(){
     return(
       <div>
-        <h1>web</h1>
+        <h1>{this.props.title}</h1>
 
       </div>
     );
@@ -26,7 +26,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="동적"></Subject>
         <Nav></Nav>
         hello
       </div>
