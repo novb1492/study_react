caa7fdaabafd7d84d122f2b46470ce954ea06a0e
kimjunyoung <kimjy80@sesisoft.com>
2022-01-11 14:35:06 +0900

    state+props+ 반복문 미쳤네

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ class App extends Component{
   constructor(props){//초기화 state 사용하려면 이렇게 해야한다
     super(props);
     this.state={
-      suject:{title:'title',sub:'www'}
+      suject:{title:'title',sub:'www'},
+      navs:[{id:1,title:'subtitle'},{id:2,title:'subtitle2'}]
     }
   }
   render(){
     return (
       <div className="App">
         <Subject title={this.state.suject.title}></Subject>
-        <Nav one="11"></Nav>
+        <Nav one={this.state.navs}></Nav>
         hello
       </div>
     );
--- src/components/nav.js
+++ src/components/nav.js
@@ -2,10 +2,14 @@ import { Component } from 'react/cjs/react.development';
 
 class Nav extends Component{
     render(){
+        var data=this.props.one;
+        var lists=[];
+        for(var i=0;i<data.length;i++){
+            lists.push(<li>{data[i].id}{data[i].title}</li>);
+        }
       return(
         <ul>
-          <li>{this.props.one}</li>
-          <li>2</li>
+          {lists}
         </ul>
       );
     }
