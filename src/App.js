b29a8fc238457aa09cfc5009764c37cfc81ef88b
kimjunyoung <kimjy80@sesisoft.com>
2022-01-11 14:00:50 +0900

    기본적인컴포넌트 넣는법

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.development';
+class Subject extends Component{
+  render(){
+    return(
+      <div>
+        <h1>web</h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
+class Nav extends Component{
+  render(){
+    return(
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    );
+  }
+}
+//여기가 app.vue 페이지가바뀌는곳
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+        hello
+      </div>
+    );
+  }
+}
 export default App;
