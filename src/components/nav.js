5ce72316b4f8076e11aed7af8da5a7b1122f6775
kimjunyoung <kimjy80@sesisoft.com>
2022-01-11 14:37:51 +0900

    반복문시 키값필요

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ class Nav extends Component{
         var data=this.props.one;
         var lists=[];
         for(var i=0;i<data.length;i++){
-            lists.push(<li>{data[i].id}{data[i].title}</li>);
+            lists.push(<li key={i}>{data[i].id}{data[i].title}</li>);//리액트는 반복문시 고유의 키값을 줘야함 
         }
       return(
         <ul>
